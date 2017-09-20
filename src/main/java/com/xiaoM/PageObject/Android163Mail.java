c96107f81183530e5f3254bddbad9b622813da1f
xmzhouk <xmzhouk@XMZHOUK-PC.issamwayodc.com>
2017-09-20 17:30:52 +0800

    添加javaCV图片识别操作

--- src/main/java/com/xiaoM/PageObject/Android163Mail.java
+++ src/main/java/com/xiaoM/PageObject/Android163Mail.java
@@ -3,6 +3,7 @@ package com.xiaoM.PageObject;
 import org.openqa.selenium.By;
 
 import com.xiaoM.Utils.ElementAction;
+import com.xiaoM.Utils.ImageCompare;
 
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
@@ -12,7 +13,8 @@ public class Android163Mail {
 	public static boolean Login163(AppiumDriver <MobileElement> driver,String userName,String passWord){
 		ElementAction.waitForElement(driver, By.id("com.netease.mail:id/editor_email"), 15).sendKeys(userName);
 		ElementAction.waitForElement(driver, By.id("com.netease.mail:id/editor_password"), 15).sendKeys(passWord);
-		ElementAction.waitForElement(driver, By.id("com.netease.mail:id/button_login"), 15).click();
+		ImageCompare.imageClick(driver, "denglu2");
+//		ElementAction.waitForElement(driver, By.id("com.netease.mail:id/button_login"), 15).click();
 		ElementAction.waitForElement(driver, By.id("com.netease.mail:id/btn_enter_mail"), 15).click();
 		return true;
 	}
